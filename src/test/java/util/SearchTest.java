51a18dc8ffd87ae7ff4b89177042eabe42424299
ssmylh <ssmylh@gmail.com>
2015-09-21 14:34:08 +0900

    Chapter 11.4 make irrevant information obvious

--- src/test/java/util/SearchTest.java
+++ src/test/java/util/SearchTest.java
@@ -15,6 +15,7 @@ import java.util.logging.Level;
 import org.junit.Test;
 
 public class SearchTest {
+    private static final String ANY_TITLE = "1";
     @Test
     public void testSearch() throws Exception {
         String pageContent = "There are certain queer times and occasions "
@@ -25,20 +26,20 @@ public class SearchTest {
         byte[] bytes = pageContent.getBytes();
         ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
         // search
-        Search search = new Search(stream, "practical joke", "1");
+        Search search = new Search(stream, "practical joke", ANY_TITLE);
         Search.LOGGER.setLevel(Level.OFF);
         search.setSurroundingCharacterCount(10);
         search.execute();
         assertFalse(search.errored());
         assertThat(search.getMatches(), is(containsMatches(
-                new Match[] { new Match("1", "practical joke", "or a vast practical joke, though t") })));
+                new Match[] { new Match(ANY_TITLE, "practical joke", "or a vast practical joke, though t") })));
 
         stream.close();
 
         // negative
         URLConnection connection = new URL("http://bit.ly/15sYPA7").openConnection();
         InputStream inputStream = connection.getInputStream();
-        search = new Search(inputStream, "smelt", "http://bit.ly/15sYPA7");
+        search = new Search(inputStream, "smelt", ANY_TITLE);
         search.execute();
         assertTrue(search.getMatches().isEmpty());
         stream.close();
