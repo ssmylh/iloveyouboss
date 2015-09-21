111e6a962d71f4f7cc4192b0e7e0d968ed07ec20
ssmylh <ssmylh@gmail.com>
2015-09-21 15:08:49 +0900

    Chapter 11.8 add blank lines to clarify 'AAA' structure

--- src/test/java/util/SearchTest.java
+++ src/test/java/util/SearchTest.java
@@ -38,10 +38,11 @@ public class SearchTest {
                 + "takes this whole universe for a vast practical joke, "
                 + "though the wit thereof he but dimly discerns, and more "
                 + "than suspects that the joke is at nobody's expense but " + "his own.");
-
         Search search = new Search(stream, "practical joke", ANY_TITLE);
         search.setSurroundingCharacterCount(10);
+
         search.execute();
+
         //assertFalse(search.errored()); TODO adds test later.
         assertThat(search.getMatches(), is(containsMatches(
                 new Match[] { new Match(ANY_TITLE, "practical joke", "or a vast practical joke, though t") })));
@@ -56,7 +57,9 @@ public class SearchTest {
         URLConnection connection = new URL("http://bit.ly/15sYPA7").openConnection();
         stream = connection.getInputStream();
         Search search = new Search(stream, "smelt", ANY_TITLE);
+
         search.execute();
+
         assertTrue(search.getMatches().isEmpty());
     }
 }
