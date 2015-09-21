4ef0bc34e285351e090ecfa4c5fd1d51772e227a
ssmylh <ssmylh@gmail.com>
2015-09-21 14:03:26 +0900

    Chapter 11.2 remove unnecessary code.

--- src/test/java/util/SearchTest.java
+++ src/test/java/util/SearchTest.java
@@ -16,39 +16,33 @@ import org.junit.Test;
 
 public class SearchTest {
     @Test
-    public void testSearch() {
-        try {
-            String pageContent = "There are certain queer times and occasions "
-                    + "in this strange mixed affair we call life when a man "
-                    + "takes this whole universe for a vast practical joke, "
-                    + "though the wit thereof he but dimly discerns, and more "
-                    + "than suspects that the joke is at nobody's expense but " + "his own.";
-            byte[] bytes = pageContent.getBytes();
-            ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
-            // search
-            Search search = new Search(stream, "practical joke", "1");
-            Search.LOGGER.setLevel(Level.OFF);
-            search.setSurroundingCharacterCount(10);
-            search.execute();
-            assertFalse(search.errored());
-            List<Match> matches = search.getMatches();
-            assertThat(matches, is(notNullValue()));
-            assertTrue(matches.size() >= 1);
-            Match match = matches.get(0);
-            assertThat(match.searchString, equalTo("practical joke"));
-            assertThat(match.surroundingContext, equalTo("or a vast practical joke, though t"));
-            stream.close();
+    public void testSearch() throws Exception {
+        String pageContent = "There are certain queer times and occasions "
+                + "in this strange mixed affair we call life when a man "
+                + "takes this whole universe for a vast practical joke, "
+                + "though the wit thereof he but dimly discerns, and more "
+                + "than suspects that the joke is at nobody's expense but " + "his own.";
+        byte[] bytes = pageContent.getBytes();
+        ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
+        // search
+        Search search = new Search(stream, "practical joke", "1");
+        Search.LOGGER.setLevel(Level.OFF);
+        search.setSurroundingCharacterCount(10);
+        search.execute();
+        assertFalse(search.errored());
+        List<Match> matches = search.getMatches();
+        assertTrue(matches.size() >= 1);
+        Match match = matches.get(0);
+        assertThat(match.searchString, equalTo("practical joke"));
+        assertThat(match.surroundingContext, equalTo("or a vast practical joke, though t"));
+        stream.close();
 
-            // negative
-            URLConnection connection = new URL("http://bit.ly/15sYPA7").openConnection();
-            InputStream inputStream = connection.getInputStream();
-            search = new Search(inputStream, "smelt", "http://bit.ly/15sYPA7");
-            search.execute();
-            assertThat(search.getMatches().size(), equalTo(0));
-            stream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail("exception thrown in test" + e.getMessage());
-        }
+        // negative
+        URLConnection connection = new URL("http://bit.ly/15sYPA7").openConnection();
+        InputStream inputStream = connection.getInputStream();
+        search = new Search(inputStream, "smelt", "http://bit.ly/15sYPA7");
+        search.execute();
+        assertThat(search.getMatches().size(), equalTo(0));
+        stream.close();
     }
 }
