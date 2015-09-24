ab3c049f515d095573608240c439703de81bd871
ssmylh <ssmylh@gmail.com>
2015-09-24 13:35:54 +0900

    Chapter 12.3 Adding Another Increment

--- src/main/java/tdd/Profile.java
+++ src/main/java/tdd/Profile.java
@@ -1,9 +1,13 @@
 package tdd;
 
 public class Profile {
+    private Answer answer;
 
     public boolean matches(Criterion criterion) {
-        return false;
+        return answer != null;
     }
 
+    public void add(Answer answer) {
+        this.answer = answer;
+    }
 }
--- src/test/java/tdd/ProfileTest.java
+++ src/test/java/tdd/ProfileTest.java
@@ -17,4 +17,17 @@ public class ProfileTest {
         assertFalse(result);
     }
 
+    @Test
+    public void matchesProfileContainsMatchingAnswer() {
+        Profile profile = new Profile();
+        Question question = new BooleanQuestion(1, "Relocation package ?");
+        Answer answer = new Answer(question, Bool.TRUE);
+        profile.add(answer);
+        Criterion criterion = new Criterion(answer, Weight.DontCare);
+
+        boolean result = profile.matches(criterion);
+
+        assertTrue(result);
+    }
+
 }
